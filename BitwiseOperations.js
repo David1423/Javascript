80a2d3e505ae60771d93d8fae24c7efb3a6ebe0f
David Raj Kanthi <kanthidavid@gmail.com>
2023-09-19 22:07:46 +0530

    Update BitwiseOperations.js

--- BitwiseOperations.js
+++ BitwiseOperations.js
@@ -10,32 +10,32 @@
 // // if we apply bitwise AND, it compares the bits, and if both bits are one then only 
 // // the resutant bit would be 1 otherwise 0. 
 
-// //Bitwise OR
-// console.log('Bitwise OR of (1 | 2) is:',1 | 2);
-// //Bitwise AND
-// console.log('Bitwise AND of (1 $ 2) is:',1 & 2);
+//Bitwise OR
+ console.log('Bitwise OR of (1 | 2) is:',1 | 2);
+//Bitwise AND
+ console.log('Bitwise AND of (1 $ 2) is:',1 & 2);
 
-// //Exercise
+//Exercise
 
-// //Read, Write, Execute
-// //00000100 = 4
-// //00000010 = 2
-// //00000001 = 1
+//Read, Write, Execute
+//00000100 = 4
+//00000010 = 2
+//00000001 = 1
 
-// const readPermission = 4;
-// const writePersmission = 2;
-// const executePermission = 1;
+const readPermission = 4;
+const writePersmission = 2;
+const executePermission = 1;
 
-// let myPermission = 0;
-// //with below bitwise OR we are actually adding the read and write permission to myPermission.
-// myPermission = myPermission | readPermission | writePersmission ;
+let myPermission = 0;
+//with below bitwise OR we are actually adding the read and write permission to myPermission.
+myPermission = myPermission | readPermission | writePersmission ;
 
-// //Using Bitwise AND for checking if we have read persmission
-// let checkReadPermission = myPermission & readPermission ? 'yes' : 'No' ;
-// console.log('Does have Read permission ?',checkReadPermission);
+//Using Bitwise AND for checking if we have read persmission
+let checkReadPermission = myPermission & readPermission ? 'yes' : 'No' ;
+console.log('Does have Read permission ?',checkReadPermission);
 
-// let checkWritePermission = myPermission & writePersmission ? 'Yes' : 'No';
-// console.log('Does have Write Permission ?', checkWritePermission);
+let checkWritePermission = myPermission & writePersmission ? 'Yes' : 'No';
+console.log('Does have Write Permission ?', checkWritePermission);
 
-// let checkExecutePermission = myPermission & executePermission ? 'Yes' : 'No';
-// console.log('Does have Execute Permission ?', checkExecutePermission);
+let checkExecutePermission = myPermission & executePermission ? 'Yes' : 'No';
+console.log('Does have Execute Permission ?', checkExecutePermission);
